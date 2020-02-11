f31130bd59798c2a7195542ab9af683f91bd4095
Kacper <kacperelwart@gmail.com>
2020-02-11 22:46:18 +0100

    Small changes
    Small changes

---
+++ Data/Migrations/20200211212140_migracja.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Terminarz.Data.Migrations
+{
+    public partial class migracja : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Happenings",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Happenings",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Happenings");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Happenings");
+        }
+    }
+}
